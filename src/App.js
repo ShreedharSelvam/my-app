ae1135e0c9fd67b3cfb8a4f103d231c7272f2730
ShreedharSelvam <shreed.max@gmail.com>
2022-12-14 21:15:14 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import React, {Component} from 'react';
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap'
 import Menu from './components/MenuComponent'
+import { DISHES, Dishes } from './shared/dishes'
+import { render } from '@testing-library/react'
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color='primary'>
-        <div className='container'>
-          <NavbarBrand href='/'>Shreedhar React App</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component() {
+  constructor(props) {
+    super(props);
+        this.state = {
+          dishes: DISHES
+        };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color='primary'>
+          <div className='container'>
+            <NavbarBrand href='/'>Shreedhar React App</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
